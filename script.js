84331a407a081319c4529fe8948a237fc7ba6cab
Shashidharak89 <shashidharak334@gmail.com>
2024-09-08 23:23:13 +0530

    color change

--- script.js
+++ script.js
@@ -3,11 +3,3 @@ function toggleSection(sectionId) {
     sections.forEach(section => section.classList.add('hidden')); // Hide all sections
     document.getElementById(sectionId).classList.remove('hidden'); // Show selected section
 }
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
-  }
-  function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
-  }

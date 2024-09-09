d9e145f764097192b2aac30ac6418c2015f325d0
Shashidharak89 <shashidharak334@gmail.com>
2024-09-09 21:11:17 +0530

    nav bar responsive modification

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+const sideItems = document.querySelectorAll('.sideitem');
+
+// Add a click event listener to each of them
+sideItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'none'
+    });
+});
+
 function toggleSection(sectionId) {
     const sections = document.querySelectorAll('.content > div'); // Select all sections
     sections.forEach(section => section.classList.add('hidden')); // Hide all sections
     document.getElementById(sectionId).classList.remove('hidden'); // Show selected section
 }
+
+function toggleSections(sectionId) {
+  const sections = document.querySelectorAll('.content > div'); // Select all sections
+  sections.forEach(section => section.classList.add('hidden')); // Hide all sections
+  document.getElementById(sectionId).classList.remove('hidden'); // Show selected section
+
+}
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
